a75e73dc984b6e97144849fbee2cb9396a647cac
Fernando Predes <fernando_predes@hotmail.com>
2022-09-06 12:55:18 -0300

    colcoado os usestates do cadastro

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -2,25 +2,36 @@ import styles from "./Register.module.css"
 import { useState, useEffect } from "react";
 
 const Register = () => {
+
+  const [displayName, setDisplayName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
   return (
     <div>
       <h1>Cadastre-se</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>
           <span>Nome:</span>
-          <input type="text" name="displayName" required placeholder="Nome do usuário" />
+          <input type="text" name="displayName" required placeholder="Nome do usuário" value={displayName} onChange={(e)=>setDisplayName(e.target.value)} />
         </label>
         <label>
           <span>E-mail:</span>
-          <input type="email" name="email" required placeholder="E-mail do usuário" />
+          <input type="email" name="email" required placeholder="E-mail do usuário" value={email} onChange={(e)=>setEmail(e.target.value)} />
         </label>
         <label>
           <span>Senha:</span>
-          <input type="password" name="password" required placeholder="Senha do usuário" />
+          <input type="password" name="password" required placeholder="Senha do usuário" value={password} onChange={(e)=>setPassword(e.target.value)} />
         </label>
         <label>
           <span>Confirmação da senha:</span>
-          <input type="password" name="confirmPassword" required placeholder="Confirme a sua senha" />
+          <input type="password" name="confirmPassword" required placeholder="Confirme a sua senha" value={confirmPassword} onChange={(e)=>setConfirmPassword(e.target.value)} />
         </label>
         <button className="btn">Cadastrar</button>
       </form>
