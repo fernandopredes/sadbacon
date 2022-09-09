55de9f7e09cdd869551864d2aa82fe8005d88e8a
Fernando Predes <fernando_predes@hotmail.com>
2022-09-09 17:23:44 -0300

    teste do useeffect no app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import './App.css';
 
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { onAuthStateChanged } from 'firebase/auth';
+
+import { useState, useEffect } from 'react';
+import { useAuthentication } from './hooks/useAuthentication';
+
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
 import Navbar from './components/Navbar';
@@ -14,6 +19,25 @@ import { AuthProvider } from './context/AuthContext';
 //pages
 
 function App() {
+const [user, setUser] = useState(undefined)
+const {auth} = useAuthentication
+
+const loadingUser = user === undefined
+
+
+
+  useEffect(() => {
+
+    onAuthStateChanged(auth,(user)=>{
+      setUser(user)
+    })
+
+  },[auth])
+
+  if (loadingUser) {
+    return <p>Carregando um belo Bacon...</p>
+  }
+
   return (
     <div className="App">
       <AuthProvider>
