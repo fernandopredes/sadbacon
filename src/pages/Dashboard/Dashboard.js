7bd7a0269b79218dffe50265bb518b4021573e27
fernando predes <fernando_predes@hotmail.com>
2022-09-21 16:02:06 -0300

    estrutura do dashboard

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,9 +1,34 @@
 import styles from './Dashboard.module.css'
 
+import { Link } from "react-router-dom";
+
+//hooks
+
+import { useAuthValue } from "../../context/AuthContext";
+import { useFetchDocuments } from "../../hooks/useFetchDocuments";
+
 const Dashboard = () => {
+  const {user} = useAuthValue()
+  const uid = user.uid
+
+  //posts do usuário
+  const posts = []
+
+
   return (
     <div>
-      <h1>Dashboard</h1>
+      <h2>Dashboard</h2>
+      <p>Gerenciamento de posts</p>
+      {posts && posts.length === 0 ? (
+        <div className={styles.noposts}>
+          <p>Não foram encontrados posts</p>
+          <Link to="/posts/create" className='btn'>Criar primeiro post</Link>
+        </div>
+      ) : (
+        <div>
+
+        </div>
+      )}
     </div>
   )
 }
