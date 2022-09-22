43b6586f0111e20fc907cd71ce78e05d7366ee92
Fernando Predes <fernando_predes@hotmail.com>
2022-09-22 12:32:13 -0300

    css do dashboard

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -22,7 +22,7 @@ const Dashboard = () => {
   }
 
   return (
-    <div>
+    <div className={styles.dashboard}>
       <h2>Dashboard</h2>
       <p>Gerenciamento de posts</p>
       {posts && posts.length === 0 ? (
@@ -32,12 +32,12 @@ const Dashboard = () => {
         </div>
       ) : (
         <>
-          <div>
+          <div className={styles.post_header}>
             <span>Título</span>
             <span>Ações</span>
 
           </div>
-          {posts && posts.map((post) => <div key={post.id}>
+          {posts && posts.map((post) => <div key={post.id} className={styles.post_row}>
             <p>{post.title}</p>
             <div>
               <Link to={`/posts/${post.id}`} className="btn btn-outline">Ver</Link>
