d3efbb1579bafc910f02cd692e20c0dcd2a75b60
fernando predes <fernando_predes@hotmail.com>
2022-09-29 21:56:36 -0300

    texto modificado

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -7,7 +7,7 @@ function About() {
     <div className={styles.about}>
       <h2>Sobre o Sad Bacon</h2>
       <p>Projeto realizado baseado no curso de React do Hora de Codar.</p>
-      <p>Foi utilizado React no Front-end e Firebase para o back-end.</p>
+      <p>Foi utilizado React no Front-end e Firebase para o Back-end.</p>
       <Link to="/posts/create" className="btn">Criar Post</Link>
     </div>
   )
